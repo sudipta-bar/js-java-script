19243ec4dd329d3ccd0d7a48afe5899f2da615f7
sudipta-bar <barsudipta579@gmail.com>
2025-07-27 09:31:01 +0000

    data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+//alert(3+3) //we are using nodejs, not browser
+let name = "sudipta"
+let age = 25
+let is =false
+let state;
+//null => standalone value,
+
+console.log (typeof undefined);
+console.log(typeof null);
+
