941ff258df996c75d5ddce6cd4d4b9a07fa5ab0c
sudipta-bar <barsudipta579@gmail.com>
2025-07-27 17:25:18 +0000

    complete

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+let score ="sudipta"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+let isLoggedIn = "sudipta"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************* operation **********
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = "sudipta"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
+
