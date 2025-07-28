a143aa8e8ca45bddfc6dcc2f9a0e2e5f4dc2199c
sudipta-bar <barsudipta579@gmail.com>
2025-07-28 10:09:03 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,23 @@
+let myDate = new Date()
+// console.log(myDate.getDate());
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2025,6,28)
+// let myCreateDate = new Date(2025,6,28,15,15)
+let myCreateDate = new Date("07-28-2025")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myCreateDate);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay()+1);
+
