c2e764c1cf09a382f728d83ea200f05aafa1e6a9
sudipta-bar <barsudipta579@gmail.com>
2025-07-30 15:12:18 +0000

    switch_case complete

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = "march"
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
