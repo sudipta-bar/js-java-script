0b1e467c49670ef72c0da7d725025d956ec9b4bf
sudipta-bar <barsudipta579@gmail.com>
2025-09-06 18:29:01 +0000

    inheritance

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,29 @@
+class user {
+    constructor(username){
+        this.username = username;
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`)
+    }
+}
+
+class Teacher extends user{
+    constructor(username,email,password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    
+    addCourse(){
+        console.log(`a new course was added by ${this.username}`)
+    }
+}
+
+const chai = new Teacher("chai","chai@gmail.com","123")
+
+chai.logMe()
+const masalachai = new user("masalachai")
+
+masalachai.logMe()
+
+console.log(chai instanceof user)
